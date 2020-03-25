e0281596a42d7731296cb67666d924ed838c6ee8
Denis Sushentsev <sushentsevds@gmail.com>
2020-03-25 17:41:21 +0300

    Added raw signup and login forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login";
+import SignUp from "./components/signup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <Router>
+        <div className="App">
+        <div className="auth-wrapper">
+        <div className="auth-inner">
+
+        <div className="navigation-links">
+        <ul class="nav nav-pills nav-fill">
+        <li class="nav-item">
+        <a class="nav-link active" data-toggle="pill" href={"/login"}>Login</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link " data-toggle="pill" href={"/sign-up"}>Sign Up</a>
+    </li>
+    </ul>
     </div>
-  );
+
+    <Switch>
+    <Route exact path='/' component={Login} />
+    <Route path="/login" component={Login} />
+    <Route path="/sign-up" component={SignUp} />
+    </Switch>
+
+    </div>
+    </div>
+    </div>
+    </Router>
+);
 }
 
 export default App;
