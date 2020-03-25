8618a13cbe5bbcee11e712338015a2ee694f556a
ArcSe <zhukova1998@gmail.com>
2020-03-25 14:06:55 +0300

    add spring starter and change modules

---
+++ backend/src/main/java/com/example/tripguide/TripGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripGuideApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/tripguide/TripGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
