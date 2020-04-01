a1c3f94f06cd404616c071be8131a2bce3cf7628
Denis Sushentsev <sushentsevds@gmail.com>
2020-04-01 18:43:51 +0300

    Enabled configuration properties

--- backend/src/main/java/com/example/tripguide/TripGuideApplication.java
+++ backend/src/main/java/com/example/tripguide/TripGuideApplication.java
@@ -1,9 +1,12 @@
 package com.example.tripguide;
 
+import com.example.tripguide.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class TripGuideApplication {
 
 	public static void main(String[] args) {
