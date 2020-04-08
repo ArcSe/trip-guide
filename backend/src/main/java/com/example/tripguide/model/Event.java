5a07866a4e80149b7318243dfe28276a4a020d88
Zhukova Iuliia <zhukova1998@gmail.com>
2020-04-08 22:53:15 +0300

    add class Event

---
+++ backend/src/main/java/com/example/tripguide/model/Event.java
@@ -0,0 +1,58 @@
+package com.example.tripguide.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Events")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Float rating;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private String address;
+
+
+    public Event() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+}
