27d6bf97ba98d08095411c99ad4078dd6cea116f
Denis Sushentsev <sushentsevds@gmail.com>
2020-04-10 17:39:14 +0300

    Added password confirmation to backend

--- backend/src/main/java/com/example/tripguide/payload/SignUpRequest.java
+++ backend/src/main/java/com/example/tripguide/payload/SignUpRequest.java
@@ -2,19 +2,31 @@ package com.example.tripguide.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class SignUpRequest {
 
-    @NotBlank
+    @NotBlank(message = "Имя не может быть пустым")
     private String name;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Email должен быть валидным")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 6, message = "Пароль должен иметь не менее 6 символов")
     private String password;
+
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 6, message = "Пароль должен иметь не менее 6 символов")
+    private String confirmPassword;
+
+    @AssertTrue(message = "Пароль подтвержден неверно")
+    private boolean isPasswordValid() {
+        return this.password.equals(this.confirmPassword);
+    }
 }
