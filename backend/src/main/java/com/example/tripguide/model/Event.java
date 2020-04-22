ff779f8e6e769b60caec469e2c7b95e57d6ed652
Denis Sushentsev <sushentsevds@gmail.com>
2020-04-22 17:56:10 +0300

    Added unidirectional connection instead of bidirectional connection to user-event

--- backend/src/main/java/com/example/tripguide/model/Event.java
+++ backend/src/main/java/com/example/tripguide/model/Event.java
@@ -43,7 +43,4 @@ public class Event {
     @JoinColumn(name = "category_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Category category;
-
-    @ManyToMany(mappedBy = "events")
-    private Set<User> users;
 }
